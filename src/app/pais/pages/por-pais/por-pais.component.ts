2628c7b9d81405db26df3d93d963cf97dd7ef1ad
edwin <edwinaguilarariza1@yahoo.com>
2021-07-26 18:48:01 -0500

    buscar por capital,y ver caracteristicas

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,21 +12,29 @@ export class PorPaisComponent  {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor( private paisService: PaisService ) { }
 
-  buscar(){
+  buscar(termino:string){
     
-    console.log(this.termino);
-
-    this.paisService.buscarPais( this.termino )
+    this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarPais(  termino )
     .subscribe( (paises) => {
-      
       console.log( paises );  
-    },( err )=>{
+      this.paises = paises;
+    },( err )=>{  
       this.hayError = true; 
+      this.paises = [];
     });
-
+  
   }
+  sugerencias( termino:string){
+    this.hayError = false;
+  }
+
+
+
 
 }
