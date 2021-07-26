ea8e23cb2a264d9f12e286b6af9f4aa66b32675a
edwin <edwinaguilarariza1@yahoo.com>
2021-07-26 13:38:55 -0500

    creacion ,enrutado ,consumo de API

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent  {
+
+  termino: string = '';
+  hayError: boolean = false;
+
+  constructor( private paisService: PaisService ) { }
+
+  buscar(){
+    
+    console.log(this.termino);
+
+    this.paisService.buscarPais( this.termino )
+    .subscribe( (paises) => {
+      
+      console.log( paises );  
+    },( err )=>{
+      this.hayError = true; 
+    });
+
+  }
+
+}
