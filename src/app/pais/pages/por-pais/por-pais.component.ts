35c615ebc98af8555b735f2f852b76301ac30a8d
edwin <edwinaguilarariza1@yahoo.com>
2021-07-27 16:50:49 -0500

    creacion de sugerencias para buscador

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor:pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent  {
@@ -13,6 +18,8 @@ export class PorPaisComponent  {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor( private paisService: PaisService ) { }
 
@@ -32,9 +39,22 @@ export class PorPaisComponent  {
   }
   sugerencias( termino:string){
     this.hayError = false;
-  }
+    this.termino = termino;
+    this.mostrarSugerencias = false;
+    
+    this.paisService.buscarPais( termino )
+    .subscribe(
+      paises => this.paisesSugeridos = paises.splice(0,5),
+      (err) => this.paisesSugeridos = []
+  );
 
+}
 
+buscarSugerido(termino: string){
+  this.buscar(termino)
+ 
+}
 
 
 }
+
